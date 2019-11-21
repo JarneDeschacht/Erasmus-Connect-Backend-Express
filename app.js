9b9b1d66721477796a14151fdb1bd3e48eb4a3c9
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2019-11-21 13:59:36 +0100

    Project setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
+
+app.listen(8080);
