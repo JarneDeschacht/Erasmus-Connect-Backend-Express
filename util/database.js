7c0f845901fadbe5287127cd25a8e721b87bb5d0
TijsM <tijs.martens@student.hogent.be>
2019-11-25 16:47:56 +0100

    added online database

--- util/database.js
+++ util/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'erasmus-connect'
+    host: 'sql7.freemysqlhosting.net',
+    user: 'sql7313407',
+    password: 'MJ1iTPx2vr',
+    database: 'sql7313407'
 });
 
 module.exports = pool.promise();
